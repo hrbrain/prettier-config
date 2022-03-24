1a2b24e431371f59d5cc885a609c4745bf9877f9
hrb-yamamoto-katsuya <yamamoto.katsuya@hrbrain.co.jp>
2022-03-24 23:47:08 +0900

    chore: change publish command

--- ship.config.js
+++ ship.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publishCommand: () =>
-    `npm_config_registry=https://npm.pkg.github.com npm publish --access public`,
+  publishCommand: ({ tag }) =>
+    `npm_config_registry=https://npm.pkg.github.com npm publish --access public --tag ${tag}`,
   buildCommand: () => null,
 };
